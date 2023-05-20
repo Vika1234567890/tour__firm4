699ec7f365b83089360021322b47610ef7e61b78
Vika1234567890 <127088512+Vika1234567890@users.noreply.github.com>
2023-05-20 19:43:14 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 // slider
-$('.multiple-items').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-});
 // $('.multiple-items').slick({
-//   dots: true,
 //   infinite: true,
-//   speed: 500,
-//   fade: true,
-//   cssEase: 'linear'
+//   slidesToShow: 3,
+//   slidesToScroll: 3,
 // });
+$('.multiple-items').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
 
 
 
